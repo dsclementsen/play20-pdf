85c9c167a3dbf69f200addc7fabc7db36048f773
Igor Laborie <ilaborie@gmail.com>
2013-10-12 09:41:26 +0200

    Update to Play 2.2.0 with Scala 2.10.3

--- module/project/Build.scala
+++ module/project/Build.scala
@@ -1,20 +1,23 @@
 import sbt._
 import Keys._
-import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "pdf"
-    val appVersion      = "0.5"
+  val appName = "pdf"
+  val appVersion = "0.6"
 
-    val appDependencies = Seq(
-      "org.xhtmlrenderer" % "core-renderer" % "R8",
-      "net.sf.jtidy" % "jtidy" % "r938"
-	)
+  // Dependencies
+  val xhtmlrenderer = "org.xhtmlrenderer" % "core-renderer" % "R8"
+  val jtidy = "net.sf.jtidy" % "jtidy" % "r938"
 
-    val main =play.Project(appName, appVersion, appDependencies).settings(
-      // hack to suppress javadoc error, see: https://play.lighthouseapp.com/projects/82401/tickets/898-javadoc-error-invalid-flag-g-when-publishing-new-module-local#ticket-898-7
-      publishArtifact in(Compile, packageDoc) := false
-    )
+  val appDependencies = Seq(
+    xhtmlrenderer,
+    jtidy
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // hack to suppress javadoc error, see: https://play.lighthouseapp.com/projects/82401/tickets/898-javadoc-error-invalid-flag-g-when-publishing-new-module-local#ticket-898-7
+    publishArtifact in(Compile, packageDoc) := false
+  )
 
 }
