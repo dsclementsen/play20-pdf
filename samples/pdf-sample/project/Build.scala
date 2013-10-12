3093a7486b63acafed6f54e85f407bc359106a30
Igor Laborie <ilaborie@gmail.com>
2013-10-12 09:53:00 +0200

    Update Sample for Play 2.2.0

--- samples/pdf-sample/project/Build.scala
+++ samples/pdf-sample/project/Build.scala
@@ -1,18 +1,19 @@
 import sbt._
 import Keys._
-import play.Project._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "pdf-sample"
-    val appVersion      = "0.2"
+  val appName = "pdf-sample"
+  val appVersion = "0.3"
 
-    val appDependencies = Seq(
-      "pdf" % "pdf_2.10" % "0.5"
-    )
+  val pdf = "pdf" % "pdf_2.10" % "0.6"
 
-    val main = play.Project(appName, appVersion, appDependencies).settings(
-      resolvers += Resolver.url("My GitHub Play Repository", url("http://www.joergviola.de/releases/"))(Resolver.ivyStylePatterns)
-    )
+  val appDependencies = Seq(
+    pdf
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    resolvers += Resolver.url("My GitHub Play Repository", url("http://www.joergviola.de/releases/"))(Resolver.ivyStylePatterns)
+  )
 
 }
